9a09360a3e4c085d544fda18e96cb631008d03dd
Ricky Board <79424125+rboard321@users.noreply.github.com>
2025-07-17 19:49:47 -0400

    Add authentication context and login/signup pages

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDFUocbiDzZozObfgBszx7WgyZY5WIrHiA",
-    authDomain: "life-skills-app-897ec.firebaseapp.com",
-    projectId: "life-skills-app-897ec",
-    storageBucket: "life-skills-app-897ec.firebasestorage.app",
-    messagingSenderId: "86291213691",
-    appId: "1:86291213691:web:5d2f0a4b48fbcc29e721de",
-    measurementId: "G-8ZPF0PWFT2"
+  apiKey: 'AIzaSyDFUocbiDzZozObfgBszx7WgyZY5WIrHiA',
+  authDomain: 'life-skills-app-897ec.firebaseapp.com',
+  projectId: 'life-skills-app-897ec',
+  storageBucket: 'life-skills-app-897ec.firebasestorage.app',
+  messagingSenderId: '86291213691',
+  appId: '1:86291213691:web:5d2f0a4b48fbcc29e721de',
+  measurementId: 'G-8ZPF0PWFT2',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import AdminPage from './components/AdminPage';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/admin" element={<AdminPage />} />
-            </Routes>
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
 );
+
