252e625976c976bcc01cfca380a5cf925cc4dfbb
Ricky Board <79424125+rboard321@users.noreply.github.com>
2025-07-17 20:31:55 -0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDFUocbiDzZozObfgBszx7WgyZY5WIrHiA",
+  authDomain: "life-skills-app-897ec.firebaseapp.com",
+  projectId: "life-skills-app-897ec",
+  storageBucket: "life-skills-app-897ec.firebasestorage.app",
+  messagingSenderId: "86291213691",
+  appId: "1:86291213691:web:5d2f0a4b48fbcc29e721de",
+  measurementId: "G-8ZPF0PWFT2"
 };
 
 const app = initializeApp(firebaseConfig);
